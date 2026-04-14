89d2a823a9cea5bdb7998bb5b5c3e575b419321b
chenyu <jimmychien14@gmail.com>
2026-04-14 15:17:45 +0900

    設定完了

---
+++ Assets/EyeGazeController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class EyeGazeController : MonoBehaviour
+{
+    private OVRPlugin.EyeGazesState EyeGazeState;
+    public GameObject RightTargetObject;
+    public GameObject LeftTargetObject;
+    public Camera RightTargetCamera;
+    public Camera LeftTargetCamera;
+
+    void Update()
+    {
+        if (OVRPlugin.GetEyeGazesState(OVRPlugin.Step.Render, -1, ref EyeGazeState))
+        {
+            var LeftEyeGaze = EyeGazeState.EyeGazes[(int)OVRPlugin.Eye.Left];
+            var RightEyeGaze = EyeGazeState.EyeGazes[(int)OVRPlugin.Eye.Right];
+
+            if (LeftEyeGaze.IsValid)
+            {
+                var LeftPose = LeftEyeGaze.Pose.ToOVRPose();
+                var RightPose = RightEyeGaze.Pose.ToOVRPose();
+
+                Vector3 GazeLeftDirection = LeftPose.orientation * Vector3.forward;
+                Vector3 GazeRightDirection = RightPose.orientation * Vector3.forward;
+
+                Vector3 GazeLeftPosition = LeftTargetCamera.transform.position;
+                Vector3 GazeRightPosition = RightTargetCamera.transform.position;
+
+                if (Physics.Raycast(GazeLeftPosition, GazeLeftDirection, out RaycastHit lefthitinfo))
+                {
+                    LeftTargetObject.transform.position = lefthitinfo.point;
+                }
+                if (Physics.Raycast(GazeRightPosition, GazeRightDirection, out RaycastHit righthitinfo))
+                {
+                    RightTargetObject.transform.position = righthitinfo.point;
+                }
+            }
+        }
+    }
+}
