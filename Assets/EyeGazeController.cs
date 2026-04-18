30e222fd9bf10fc786d0a73601504800ddf95b93
chenyu <jimmychien14@gmail.com>
2026-04-18 17:38:44 +0900

    コードをOPENXRに変更

--- Assets/EyeGazeController.cs
+++ Assets/EyeGazeController.cs
@@ -1,39 +1,71 @@
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.OpenXR;
+using UnityEngine.XR.OpenXR.Features.Interactions;
+using System.Collections.Generic;
 
 public class EyeGazeController : MonoBehaviour
 {
-    private OVRPlugin.EyeGazesState EyeGazeState;
     public GameObject RightTargetObject;
     public GameObject LeftTargetObject;
     public Camera RightTargetCamera;
     public Camera LeftTargetCamera;
 
+    private InputDevice eyeTrackingDevice;
+
+    void Start()
+    {
+        // Eye Tracking デバイスを取得
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics eyeTrackingCharacteristics =
+            InputDeviceCharacteristics.EyeTracking | InputDeviceCharacteristics.TrackedDevice;
+        InputDevices.GetDevicesWithCharacteristics(eyeTrackingCharacteristics, devices);
+
+        if (devices.Count > 0)
+        {
+            eyeTrackingDevice = devices[0];
+            Debug.Log("Eye Tracking device found: " + eyeTrackingDevice.name);
+        }
+        else
+        {
+            Debug.LogWarning("Eye Tracking device not found.");
+        }
+    }
+
     void Update()
     {
-        if (OVRPlugin.GetEyeGazesState(OVRPlugin.Step.Render, -1, ref EyeGazeState))
+        if (!eyeTrackingDevice.isValid)
         {
-            var LeftEyeGaze = EyeGazeState.EyeGazes[(int)OVRPlugin.Eye.Left];
-            var RightEyeGaze = EyeGazeState.EyeGazes[(int)OVRPlugin.Eye.Right];
-
-            if (LeftEyeGaze.IsValid)
+            // デバイスが見つからない場合、再取得を試みる
+            List<InputDevice> devices = new List<InputDevice>();
+            InputDevices.GetDevicesWithCharacteristics(
+                InputDeviceCharacteristics.EyeTracking | InputDeviceCharacteristics.TrackedDevice,
+                devices);
+            if (devices.Count > 0)
             {
-                var LeftPose = LeftEyeGaze.Pose.ToOVRPose();
-                var RightPose = RightEyeGaze.Pose.ToOVRPose();
+                eyeTrackingDevice = devices[0];
+            }
+            return;
+        }
 
-                Vector3 GazeLeftDirection = LeftPose.orientation * Vector3.forward;
-                Vector3 GazeRightDirection = RightPose.orientation * Vector3.forward;
+        // 視線の位置と向きを取得
+        if (eyeTrackingDevice.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 gazePosition) &&
+            eyeTrackingDevice.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion gazeRotation))
+        {
+            Vector3 gazeDirection = gazeRotation * Vector3.forward;
 
-                Vector3 GazeLeftPosition = LeftTargetCamera.transform.position;
-                Vector3 GazeRightPosition = RightTargetCamera.transform.position;
+            // Left Eye
+            Vector3 leftRayOrigin = LeftTargetCamera.transform.position;
+            if (Physics.Raycast(leftRayOrigin, gazeDirection, out RaycastHit leftHit))
+            {
+                LeftTargetObject.transform.position = leftHit.point;
+            }
 
-                if (Physics.Raycast(GazeLeftPosition, GazeLeftDirection, out RaycastHit lefthitinfo))
-                {
-                    LeftTargetObject.transform.position = lefthitinfo.point;
-                }
-                if (Physics.Raycast(GazeRightPosition, GazeRightDirection, out RaycastHit righthitinfo))
-                {
-                    RightTargetObject.transform.position = righthitinfo.point;
-                }
+            // Right Eye
+            Vector3 rightRayOrigin = RightTargetCamera.transform.position;
+            if (Physics.Raycast(rightRayOrigin, gazeDirection, out RaycastHit rightHit))
+            {
+                RightTargetObject.transform.position = rightHit.point;
             }
         }
     }
